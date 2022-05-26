e9974be295eb51a970384a3a25610cfbaea4ff68
Tennyson Ben-kalio <officialtennysonbk@gmail.com>
2022-05-27 00:25:13 +0100

    added client-side page guards -route protection

--- components/profile/user-profile.js
+++ components/profile/user-profile.js
@@ -1,8 +1,30 @@
+import {  getSession } from 'next-auth/client';
+import { useEffect, useState } from 'react';
+
 import ProfileForm from './profile-form';
 import classes from './user-profile.module.css';
 
 function UserProfile() {
+  const [isLoading, setIsLoading] = useState(true);
+  
   // Redirect away if NOT auth
+  useEffect(() => {
+    getSession().then((session) => {
+      setIsLoading(false);
+
+      if (!session) {
+        window.location.href = '/auth';
+      }
+    })
+  }, []);
+
+  if (isLoading) {
+    return (
+      <p className='classes.profile'>
+        loading...
+      </p>
+    )
+  }
 
   return (
     <section className={classes.profile}>
